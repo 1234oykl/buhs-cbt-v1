cc4776f49e1477da4edc1432fbfbf700afe22eb8
Oyewole Olukunle <oyewoleolukunle007@gmail.com>
2026-05-01 22:36:23 -0700

    Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('Server is running...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+
+const app = express()
+
+app.get('/', (req, res) => {
+  // res.send('API is running..')
+  res.status(200).json({message: 'welcome to BUHS CBT API'})
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
