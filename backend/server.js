9647afb6949d75d79036934e95f7daf6f7d80b3e
Olukunle Oyewole <oyewoleolukunle007@gmail.com>
2026-06-03 11:34:41 +0100

    Add test route

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ const mongoose = require("mongoose");
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
+
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Result routes working",
+  });
+});
+
 const resultRoutes = require("./routes/resultRoutes");
+console.log("RESULT ROUTES LOADED");
 const examRoutes = require("./routes/examRoutes");
 console.log("EXAM ROUTES LOADED:", typeof examRoutes);
 const userRoutes = require("./routes/userRoutes");
